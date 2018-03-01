a58fb2ec602cbf32237281060f9f2bbc820a13f5
awtian <say@awtian.com>
2018-03-01 15:10:38 +0700

    layouting

--- server/app.js
+++ server/app.js
@@ -5,13 +5,16 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
 var users = require('./routes/users');
 var articles = require('./routes/articles');
-
 var app = express();
+var cors = require('cors');
+
+app.use(cors())
 mongoose.connect('mongodb://localhost/lcode')
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/users', users);
 app.use('/articles', articles);
